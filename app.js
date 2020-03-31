9d670443951e33c99e5c98c2f89761cdc6eba64f
Aryaman <aryavnps@gmail.com>
2020-03-31 16:54:26 +0530

    added campgrounds route and view

--- app.js
+++ app.js
@@ -7,6 +7,15 @@ app.get('/',function (req,res){
 	res.render('landing')
 });
 
+app.get("/campgrounds",function(req,res){
+	var campgrounds = [
+		{name: "Salmon Creek",image: "https://cdn.pixabay.com/photo/2016/11/21/14/31/vw-bus-1845719_1280.jpg"},
+		{name: "Granite Hill",image: "https://cdn.pixabay.com/photo/2016/11/22/23/08/adventure-1851092_1280.jpg"},
+		{name: "Mountain Goat's Rest",image: "https://cdn.pixabay.com/photo/2020/01/11/07/39/north-4756774_1280.jpg"}
+	]
+	res.render('campgrounds',{campgrounds:campgrounds})
+})
+
 
 
 app.listen(8000,function(){
